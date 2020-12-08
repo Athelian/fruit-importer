164f126f3011eb58d341b69b1d6846c246ad6616
Eliot <ejaustinforbes@gmail.com>
2020-12-08 09:23:25 +0900

    server with first route

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
-const db = require("./knex.js");
+const db = require("./knex");
 
 const app = express();
 
@@ -15,5 +15,16 @@ app.use(
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.get("/api/user-locations", async (req, res) => {
+  try {
+    const locations = await db.select("lat", "lng").table("users");
+    console.log("locations!");
+    res.json(locations);
+  } catch (err) {
+    console.error("Error loading locations!", err);
+    res.sendStatus(500);
+  }
+});
 
 module.exports = app;
--- server/knex.js
+++ server/knex.js
@@ -1,10 +1,15 @@
 const knex = require("knex");
 require("dotenv").config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const db = knex({
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connection:
+    "postgres://postgres:aeroplane@localhost:5432/users" ||
+    process.env.DATABASE_URL,
   searchPath: "public",
 });
+
+console.log(db.select("name").table("users"));
 
 module.exports = db;
