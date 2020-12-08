a0a7d0114128190bab2403290380bd4223fb0777
Eliot <ejaustinforbes@gmail.com>
2020-12-08 09:28:06 +0900

    process.env first condition

--- server/knex.js
+++ server/knex.js
@@ -5,8 +5,8 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const db = knex({
   client: "pg",
   connection:
-    "postgres://postgres:aeroplane@localhost:5432/users" ||
-    process.env.DATABASE_URL,
+    process.env.DATABASE_URL ||
+    "postgres://postgres:aeroplane@localhost:5432/users",
   searchPath: "public",
 });
 
